a77c4b7f519a40f633f92824b2e417edb5542970
SiriYang <www.yangxinruei@qq.com>
2019-05-07 00:54:33 +0800

    添加图相关代码

---
+++ src/map/Edge.java
@@ -0,0 +1,13 @@
+package map;
+
+public class Edge {
+    public int value;
+    public Node from;
+    public Node to;
+
+    public Edge(int value, Node from, Node to) {
+        this.value = value;
+        this.from = from;
+        this.to = to;
+    }
+}
