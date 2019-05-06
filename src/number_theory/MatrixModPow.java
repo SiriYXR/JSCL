b19b39d4c2f3ff8cd3a58aaa89796e77ef1b21ac
SiriYang <www.yangxinruei@qq.com>
2019-05-06 21:55:15 +0800

    添加矩阵快速幂代码

---
+++ src/number_theory/MatrixModPow.java
@@ -0,0 +1,39 @@
+package number_theory;
+
+public class MatrixModPow {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    public static long[][] matrixModPow(int k, int n, long[][] A) {
+        long[][] res = new long[n][n];
+        for (int i = 0; i < res.length; i++) {
+            for (int j = 0; j < res[i].length; j++) {
+                if (i == j) {
+                    res[i][j] = 1;
+                } else {
+                    res[i][j] = 0;
+                }
+            }
+        }
+        while (k != 0) {
+            if ((k & 1) == 1) res = matMult(res, A);
+            k >>= 1;//k/=2;
+            A = matMult(A, A);
+        }
+        return res;
+    }
+
+    public static long[][] matMult(long[][] A, long[][] B) {
+        long res[][] = new long[A.length][B.length];
+        for (int i = 0; i < res.length; i++) {
+            for (int j = 0; j < res[i].length; j++) {
+                for (int k = 0; k < A[0].length; k++) {
+                    res[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        return res;
+    }
+}
