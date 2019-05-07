ff8e9e76b76796479222b8807fa5edd840945f1e
SiriYang <www.yangxinruei@qq.com>
2019-05-07 08:11:51 +0800

    Create DFS.java

---
+++ src/map/DFS.java
@@ -0,0 +1,45 @@
+package map;
+
+import map.basic.Node;
+
+import java.util.HashSet;
+import java.util.Stack;
+
+public class DFS {
+
+    public static void main(String[] args) {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+
+        node1.nexts.add(node2);
+        node1.nexts.add(node3);
+        node2.nexts.add(node4);
+        node3.nexts.add(node4);
+
+        dfs(node1);
+    }
+
+    public static void dfs(Node node) {
+        if (node == null)
+            return;
+        Stack<Node> stack = new Stack<>();
+        HashSet<Node> set = new HashSet<>();
+        stack.add(node);
+        set.add(node);
+        System.out.println(node.value);//根据题目调整该行代码
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            for (Node next : cur.nexts) {
+                if (!set.contains(next)) {
+                    stack.push(cur);
+                    stack.push(next);
+                    set.add(next);
+                    System.out.println(next.value);//根据题目调整该行代码
+                    break;
+                }
+            }
+        }
+    }
+}
