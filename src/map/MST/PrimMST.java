8356cb6233c15c1b8f44cda5a9bccfaacf678530
SiriYang <www.yangxinruei@qq.com>
2019-05-07 09:53:05 +0800

    Create PrimMST.java

---
+++ src/map/MST/PrimMST.java
@@ -0,0 +1,44 @@
+package map.MST;
+
+import map.basic.Edge;
+import map.basic.Graph;
+import map.basic.Node;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+public class PrimMST {
+
+    public static class EdgeComparator implements Comparator<Edge> {
+        @Override
+        public int compare(Edge o1, Edge o2) {
+            return o1.weight - o2.weight;
+        }
+    }
+
+    public static Set<Edge> primMST(Graph graph) {
+        PriorityQueue<Edge> priorityQueue = new PriorityQueue<>(new EdgeComparator());
+        HashSet<Node> set = new HashSet<>();
+        Set<Edge> result = new HashSet<>();
+        for (Node node : graph.nodes.values()) {
+            if (!set.contains(node)) {
+                set.add(node);
+                for (Edge edge : node.edges)
+                    priorityQueue.add(edge);
+                while (!priorityQueue.isEmpty()) {
+                    Edge edge = priorityQueue.poll();
+                    Node toNode = edge.to;
+                    if (!set.contains(toNode)) {
+                        set.add(toNode);
+                        result.add(edge);
+                        for (Edge nextEdge : toNode.edges)
+                            priorityQueue.add(nextEdge);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
