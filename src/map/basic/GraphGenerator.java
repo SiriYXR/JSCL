623d9db6d44bfe0c71b1d1c41e47d267f6cd57d8
SiriYang <www.yangxinruei@qq.com>
2019-05-07 09:06:24 +0800

    修复为添加from结点的bug

--- src/map/basic/GraphGenerator.java
+++ src/map/basic/GraphGenerator.java
@@ -9,6 +9,9 @@ public class GraphGenerator {
             Integer weight = matrix[i][0];
             Integer from = matrix[i][1];
             Integer to = matrix[i][2];
+            if (!graph.nodes.containsKey(from)) {
+                graph.nodes.put(from, new Node(from));
+            }
             if (!graph.nodes.containsKey(to)) {
                 graph.nodes.put(to, new Node(to));
             }
